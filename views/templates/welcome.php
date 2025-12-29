38379602146fe29fd3eb8551ff80dbec5235a8d8
François KERVELLA <kervella.francois@gmail.com>
2025-12-29 11:39:16 +0100

    Ajout de la structure du la page d'accueil

---
+++ views/templates/welcome.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Template pour afficher la page d'accueil
+ */
+?>
+
+<div class="">
+    <h2>Rejoignez nos lecteurs passionnés</h2>
+</div>
+<div class="">
+    <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
+</div>
+<div class="">
+    Découvrir
+</div>
+<div class="">
+    <img src="img" alt="Image accueil">
+</div>
+<div class="">
+    Hamza
+</div>
+<div class="">
+    <h2>Les derniers livres ajoutés</h2>
+</div>
+<div class="">
+</div>
+<div class="">
+    Voir tous les livres
+</div>
+<div class="">
+    <h2>Comment ça marche ?</h2>
+    <p>Echanger des livres avec Tomtroc c'est simple et amusant ! Suivez ces étapes pour commencer :</p>
+    <ul>
+        <li>Inscrivez-vous gratuitement sur notre plateforme.</li>
+        <li>Ajoutez à votre profil les livres que vous souhaitez échanger.</li>
+        <li>Parcourez les livres disponibles chez d'autres membres.</li>
+        <li>Proposez un échange et discutez avec d'autres passionnés de lecture.</li>
+    </ul>
+<div class="">
+    Voir tous les livres
+</div>
+<div class="">
+    <img src="img/" alt="">
+</div>
+<div class="">
+    <h2>Nos valeurs</h2>
+</div>
+<div class="">
+    <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs.Nous croyons en la puissance des histoires pour rassemlber les gens et inspirer des conversations enrichissantes.</p>
+    <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
+    <p>Nous sommes passionés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
+</div>
+<div class="">
+L'équipe Tom Troc
+</div>
+<div class="">
+    <img src="img/" alt="">
+</div>
