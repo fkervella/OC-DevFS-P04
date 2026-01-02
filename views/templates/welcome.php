df5ee708779abdf01fd7e625b640f9f2cb0181cf
François KERVELLA <kervella.francois@gmail.com>
2026-01-02 18:40:24 +0100

    welcomePage : correction du CSS après vérification du juste nécessaire

--- views/templates/welcome.php
+++ views/templates/welcome.php
@@ -5,7 +5,6 @@
  */
 ?>
 
-<div class="page">
     <div class="introduction">
         <div class="content">
             <div class="column1">
@@ -96,7 +95,7 @@
         <div class="content">
             <h2>Nos valeurs</h2>
             <div class="text">
-                <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs.Nous croyons en la puissance des histoires pour rassemlber les gens et inspirer des conversations enrichissantes.</p>
+                <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
                 <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
                 <p>Nous sommes passionés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
             </div>
@@ -108,4 +107,3 @@
             </div>
         </div>
     </div>
-</div>
