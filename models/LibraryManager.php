3c382ef1b212022519488b717293745f8eb31389
François KERVELLA <kervella.francois@gmail.com>
2026-01-11 16:38:42 +0100

    addBook : ajout du fichier oublié : ajout du livre à la bibliothèque utilisateur

---
+++ models/LibraryManager.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * \brief gère les biliothèques des utilisateurs.
+ */
+class LibraryManager extends AbstractEntityManager
+{
+    /**
+     * Ajoute un livre à la bibliothèque d'un utilisateur.
+     *
+     * @param $userId identifiant de l'utilisatuer
+     * @param $bookId identifiant du livre
+     *
+     * @return true si l'ajout a réussi, sinon false
+     */
+    public function addBook($userId, $bookId): bool
+    {
+        $sql = 'INSERT INTO library (user_id, book_id) VALUES(:userId, :bookId)';
+        $result = $this->db->query($sql, [
+            'userId' => $userId,
+            'bookId' => $bookId,
+        ]);
+
+        return $result->rowCount() > 0;
+    }
+}
