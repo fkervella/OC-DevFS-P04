3362dce2d0bb524628d12bfab9b0486cc5531406
François KERVELLA <kervella.francois@gmail.com>
2026-01-15 01:50:09 +0100

    Mise à jour de la structure de la base de données pour l'affichage des conversations et messages

--- sql/tomtroc.sql
+++ sql/tomtroc.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 21, 2025 at 07:27 PM
+-- Generation Time: Jan 15, 2026 at 12:42 AM
 -- Server version: 11.8.3-MariaDB-0+deb13u1 from Debian
--- PHP Version: 8.4.11
+-- PHP Version: 8.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,13 +29,42 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `book` (
   `id` int(11) NOT NULL,
-  `picture` varchar(255) NOT NULL DEFAULT '',
+  `picture` varchar(255) DEFAULT NULL,
   `title` varchar(255) NOT NULL DEFAULT '',
   `author` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL DEFAULT '',
-  `user_id` int(11) DEFAULT NULL,
-  `availability` int(11) NOT NULL DEFAULT 0
+  `availability` int(11) NOT NULL DEFAULT 0,
+  `add_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `picture`, `title`, `author`, `description`, `availability`, `add_date`) VALUES
+(20, './img/books/20', 'second livre', 'moi', 'la suite de mon oeuvre', 1, '2026-01-11 14:40:13'),
+(21, NULL, 'Un livre', 'Un seul', 'Mon préféré', 1, '2026-01-12 22:17:31'),
+(27, './img/books/27', 'test', 'test1', 'test3', 1, '2026-01-13 21:58:56'),
+(28, './img/books/28', 'ytry', 'yertyter', 'yertyeye', 1, '2026-01-13 21:59:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `user_id_1` int(11) NOT NULL,
+  `user_id_2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `user_id_1`, `user_id_2`) VALUES
+(1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -48,6 +77,16 @@ CREATE TABLE `library` (
   `book_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+--
+-- Dumping data for table `library`
+--
+
+INSERT INTO `library` (`user_id`, `book_id`) VALUES
+(1, 20),
+(1, 21),
+(2, 27),
+(2, 28);
+
 -- --------------------------------------------------------
 
 --
@@ -56,11 +95,21 @@ CREATE TABLE `library` (
 
 CREATE TABLE `message` (
   `id` int(11) NOT NULL,
-  `from_user_id` int(11) NOT NULL,
-  `to_user_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
   `datetime` datetime NOT NULL,
-  `message` varchar(1024) NOT NULL
+  `message` varchar(1024) NOT NULL,
+  `viewed` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `chat_id`, `sender_id`, `datetime`, `message`, `viewed`) VALUES
+(1, 1, 2, '2026-01-15 00:52:10', 'Ceci est un test', 0),
+(2, 1, 2, '2026-01-15 00:52:36', 'Ceci est un test', 0),
+(3, 1, 2, '2026-01-15 01:41:33', 'Tapez votre message ici', 0);
 
 -- --------------------------------------------------------
 
@@ -71,11 +120,19 @@ CREATE TABLE `message` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `password` varchar(55) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `creation_date` datetime NOT NULL,
   `avatar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `pseudo`, `login`, `password`, `creation_date`, `avatar`) VALUES
+(1, 'user', 'user@a.a', '$2y$12$Wm9Hc7EdNKvokZUGrusOx.D61pi3X0ncsfs3HBzdz.gaznNYgePWq', '2026-01-03 14:25:17', 'img/user.jpg'),
+(2, 'francois', 'francois@a.a', '$2y$12$eAxJ9gHR.sif.0jOgGP5ZeLOqX/E.uqaP74hetXBqeOGDAgtEpCve', '2026-01-10 11:33:09', 'img/user.jpg');
 
 --
 -- Indexes for dumped tables
@@ -85,21 +142,29 @@ CREATE TABLE `user` (
 -- Indexes for table `book`
 --
 ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_book_user_id` (`user_id`);
+  ADD KEY `FK_CHAT_USER_ID_1` (`user_id_1`),
+  ADD KEY `FK_CHAT_UDER_ID_2` (`user_id_2`);
 
 --
 -- Indexes for table `library`
 --
 ALTER TABLE `library`
-  ADD KEY `fk_library_book_id` (`book_id`),
-  ADD KEY `fk_library_user_id` (`user_id`);
+  ADD PRIMARY KEY (`user_id`,`book_id`),
+  ADD KEY `fk_library_book_id` (`book_id`);
 
 --
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_MESSAGE_CHATID` (`chat_id`);
 
 --
 -- Indexes for table `user`
@@ -115,29 +180,36 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `book`
+-- Constraints for table `chat`
 --
-ALTER TABLE `book`
-  ADD CONSTRAINT `fk_book_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `chat`
+  ADD CONSTRAINT `FK_CHAT_UDER_ID_2` FOREIGN KEY (`user_id_2`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_CHAT_USER_ID_1` FOREIGN KEY (`user_id_1`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `library`
@@ -145,6 +217,12 @@ ALTER TABLE `book`
 ALTER TABLE `library`
   ADD CONSTRAINT `fk_library_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_library_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `FK_MESSAGE_CHATID` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
