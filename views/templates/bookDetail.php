442924551e8a304f3955e3110c52764cd7a165da
François KERVELLA <kervella.francois@gmail.com>
2026-01-15 01:49:23 +0100

    bookDetail : affichage des données issues de la base de données

--- views/templates/bookDetail.php
+++ views/templates/bookDetail.php
@@ -9,27 +9,24 @@
 </div>
 <div class="content">
     <div class="column1">
-        <img src="img/detailLivre.jpg" alt="">
+    <img src="<?php echo $book->getPicture(); ?>" alt="image du livre <?php echo $book->getTitle(); ?>">
     </div>
     <div class="column2">
-        <div class="title">The Kinfolk Table
+    <div class="title"><?php echo $book->getTitle(); ?>
         </div>
-        <div class="author">Par Nathan Williams
+            <div class="author">Par <?php echo $book->getAuthor(); ?>
         </div>
         <p class="break">___</p>
         <p class="sectionTitle">Description</p>
         <div class="description">
-            <p>J'ai récemment plongé dans les pages de 'The Kinflok Table' et j'ai été enchanté par cette oeuvre captivante. Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager des moments authentiques autour de la table.</p>
-            <p>Les photographies mangifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans un voyage à travers des recettes et des histoires qui mettent en avant la beauté de la simplicité et de la convivialité.</p>
-            <p>Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres chers.</p>
-            <p>'The Kinfold Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain que ce livre trouvera une place spéciale dans le coeur de tout amoureux de la cuisine et des rencontres inspirantes.</p>
+            <?php echo Utils::format($book->getDescription()); ?>
         </div>
         <p class="sectionTitle">Propriétaire</p>
         <div class="userIcon">
-            <img src="img/user.jpg" alt="">
-            <p>Alexlecture</p>
+        <img src="<?php echo $user->getAvatar(); ?>" alt="avatar de <?php echo $user->getPseudo(); ?>">
+            <p><?php echo $user->getPseudo(); ?></p>
         </div>
-        <a href="index.php?action=sendMessage&bookId=1">
+        <a href="index.php?action=showNewMessage&bookId=<?php echo $book->getId(); ?>">
             <div class="button">Envoyer un message
             </div>
         </a>
