b4060f091f6f4138956e46807f4ba0b5a067f0ee
François KERVELLA <kervella.francois@gmail.com>
2026-01-17 18:03:54 +0100

    Correction de l'envoi d'un message vers un utilisateur, à partir de sa page publique

--- controllers/MessageController.php
+++ controllers/MessageController.php
@@ -84,31 +84,43 @@ class MessageController
         }
 
         $userId = intval(htmlspecialchars($_SESSION['userId']));
-        $chatId = intval(htmlspecialchars(Utils::request('chatId')));
-        $bookId = intval(htmlspecialchars(Utils::request('bookId')));
 
-        $bookManager = new BookManager();
-        $book = $bookManager->getBookById($bookId);
-        $sellerId = $book->getSellerId();
+        if (!empty(Utils::request('chatId') && !empty(Utils::request('bookId'))))
+        {
+            //Cas d'une nouvelle conversation demandée depuis la page d'un livre
+            $chatId = intval(htmlspecialchars(Utils::request('chatId')));
+            $bookId = intval(htmlspecialchars(Utils::request('bookId')));
+    
+            $bookManager = new BookManager();
+            $book = $bookManager->getBookById($bookId);
+            $sellerId = $book->getSellerId();
+        }
+        else 
+        {
+            // Cas d'une nouvelle conversation demadnée depuis la page compte publique de l'utilisateur
+            $sellerId = intval(htmlspecialchars(Utils::request('userId')));
+        }
 
         // 2.
         $chatManager = new ChatManager();
         if (!$chatManager->existsChat($userId, $sellerId)) {
-            $chatManager->createChat($userId, $sellerId);
+             $chatManager->createChat($userId, $sellerId);
         }
+        else
+            $chatId = $chatManager->getChatIdByUserId($userId, $sellerId);
 
         // 3.
         $chats = $chatManager->getChatByUserId($userId);
-
+    
         $messages = null;
         $currentChat = null;
         if (!empty($chatId)) {
             // 4.
             $currentChat = $chatManager->getChatById($chatId, $userId);
-
+ 
             // 5.
             if ($chatManager->getChatMessageNumber($chatId) > 0) {
-                $messages = $chatManager->getChatMessages($chatId);
+                 $messages = $chatManager->getChatMessages($chatId);
             }
         }
 
@@ -116,7 +128,6 @@ class MessageController
         $view = new View('Messagerie');
         $view->render('chat', [
             'userId' => $userId,
-            'bookId' => $bookId,
             'chats' => $chats,
             'currentChat' => $currentChat,
             'messages' => $messages,
