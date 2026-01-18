9a807a59c25eea02dc8bd4cefc2272c1d54118ab
François KERVELLA <kervella.francois@gmail.com>
2026-01-18 20:45:27 +0100

    Page messagerie Correction du js pour affichage en responsive

--- script/chat.js
+++ script/chat.js
@@ -1,14 +1,25 @@
-document.getElementById('back').addEventListener('click', function() {
+const mediaQuery = window.matchMedia('(max-width :1000px)')
 
-    document.getElementById('column1').style.display = "flex"
-    document.getElementById('column2').style.display = "none"
-});
+function handleMediaChange(mediaQuery) {
 
-
-window.addEventListener('load', function() {
-
-    if(document.getElementById('chatId')) {
-        document.getElementById('column1').style.display = "none"
-        document.getElementById('column2').style.display = "flex"
+    if (mediaQuery.matches) {
+        document.getElementById('back').addEventListener('click', function() {
+    
+            document.getElementById('column1').style.display = "flex"
+            document.getElementById('column2').style.display = "none"
+        });
+    
+    
+        window.addEventListener('load', function() {
+        
+            if(document.getElementById('chatId')) {
+                document.getElementById('column1').style.display = "none"
+                document.getElementById('column2').style.display = "flex"
+            }
+        });
     }
-});
+}
+
+mediaQuery.addEventListener('change', handleMediaChange)
+
+handleMediaChange(mediaQuery)
